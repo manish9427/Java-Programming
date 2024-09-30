3d45a9db273a1eeb57035f3255149a20fe3a1774
manish9427 <manish119427@gmail.com>
2024-09-30 12:21:49 +0530

    new

--- Interview/Rotate2D_Inteview.java
+++ Interview/Rotate2D_Inteview.java
@@ -5,8 +5,12 @@ public class Rotate2D_Inteview {
         // {9,5,1}, {10,6,2},{11,7,3},{12,8,4}
         // AntiClockwise
         // {4,8,12}, {3,7,11},{2,6,10},{1,5,9}
+        System.out.println("Clockwise Rotation: ");
         int [][] clockwise = rotateClockwise(arr);
         printMatrix(clockwise);
+        System.out.println("AnticlockwiseRotation: ");
+        int [][] AntiClockwise = rotateAntiClockwise(arr);
+        printMatrix(AntiClockwise);
     }
     public static  int[][] rotateClockwise(int[][] matrix ){
           int rows = matrix.length;
@@ -18,6 +22,17 @@ public class Rotate2D_Inteview {
             }
           }
           return rotated;
+    }
+    public static int[][] rotateAntiClockwise(int[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int rotated[][] = new int [ cols][rows];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                rotated[cols-1-j][i]=matrix[i][j];
+            }
+        }
+        return rotated;
     }
     public static void printMatrix(int [][] arr){
         for(int[] row : arr){
