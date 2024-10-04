8fed87b9d70db62bd59e7cc76cfc8d908be08e08
manish9427 <manish119427@gmail.com>
2024-10-04 12:09:26 +0530

    new

---
+++ Interview/MergeArrayInterview.java
@@ -0,0 +1,34 @@
+public class MergeArrayInterview {
+    public static void main(String args[]){
+        int arr1[] = {1, 5, 9, 10, 15};
+        int arr2[] = {2, 3, 8, 13};
+        int m = arr1.length;
+        int n = arr2.length;
+        int []  newArray = new int[m+n];
+        int i=0,j=0,k=0;
+        while(i<m && j<n){
+            if(arr1[i]<arr2[j]){
+                newArray[k]=arr1[i];
+                k++;
+                i++;
+            }else{
+                newArray[k]=arr2[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<m){
+            newArray[k]=arr1[i];
+            k++;
+            i++;
+        }
+        while(j<n){
+            newArray[k]=arr2[j];
+            k++;
+            j++;
+        }
+        for(int ans : newArray){
+            System.out.print(ans+" ");
+        }
+    }
+}
