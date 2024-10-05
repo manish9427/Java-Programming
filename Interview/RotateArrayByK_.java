558649829cc48c54cc2e91be53858735e7d243b8
manish9427 <manish119427@gmail.com>
2024-10-05 10:47:09 +0530

    new

---
+++ Interview/RotateArrayByK_.java
@@ -0,0 +1,11 @@
+/*
+Rotate the elements of the array to the right by K positions.
+
+Example: Input: [1, 2, 3, 4, 5], K = 2
+Output: [4, 5, 1, 2, 3]
+ */
+public class RotateArrayByK_ {
+    public static void main(String args[]){
+
+    }
+}
