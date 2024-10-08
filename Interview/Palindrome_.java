11a2c801216384557355cde1fda35b678391966e
manish9427 <manish119427@gmail.com>
2024-10-08 11:25:14 +0530

    new

--- Interview/Palindrome_.java
+++ Interview/Palindrome_.java
@@ -3,15 +3,34 @@
  */
 public class Palindrome_ {
     public static void main(String args[]){
+
+        // - String
         String str = "abccba";
         String rev = "";
         for(int i=str.length()-1;i>=0;i--){
             rev += str.charAt(i);
         }
         if(rev.equals(str)){
-            System.out.println("Palindrome");
+            System.out.println(str +" is Palindrome");
         }else{
-            System.out.println("Not a Plaindrome");
+            System.out.println(str + " is Not a Plaindrome");
+        }
+
+        //  Number - 
+        int num = 121;
+        int revNum = 0;
+        int originalNum = num;
+
+        while(num>0){
+            int digit = num%10;
+            revNum = revNum * 10 + digit;
+            num = num/10;
+        }
+
+        if(revNum==originalNum){
+            System.out.println(originalNum + " is Palindrome");
+        }else{
+            System.out.println(originalNum +" is Not a Plaindrome");
         }
     }
 }
