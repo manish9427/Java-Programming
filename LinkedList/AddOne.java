d5a3bf86e7122203848c210e101da4b2925632ed
manish9427 <manish119427@gmail.com>
2024-10-26 23:57:41 +0530

    new

---
+++ LinkedList/AddOne.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+//  Node class
+class Node{
+    int data;
+    Node next;
+    // node class constructor
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
+
+public class AddOne{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Linked list(comma-seprated values): ");
+        String [] values=sc.nextLine().split(",");
+        Node head =new Node(Integer.parseInt(values[0]));
+        Node current=head;
+        for(int i=1;i<values.length;i++){
+            current.next=new Node(Integer.parseInt(values[i]));
+            current=current.next;
+        }
+        
+        //Add one to the Linked List
+        head=addOne(head);
+        
+        //print the resulting linked list
+        System.out.println("Result:- ");
+        while(head != null){
+            System.out.print(head.data);
+            head=head.next;
+            if(head!=null){
+                System.out.print(" -> ");
+            }
+        }
+    }
+
+    public static Node reverse(Node head){
+        Node current=head;
+        Node prev=null;
+        Node next=null;
+        while(current != null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+    }
+    
+    public static Node addOne(Node head){
+        Node new_head=reverse(head);
+        int carry=0;
+        Node prev=null;
+        Node current1=new_head;
+        Node curr=new_head;
+        while(curr != null){
+            int sum=0;
+            if(prev == null){
+                sum=curr.data+1;
+            }else{
+                sum=curr.data+carry;
+            }
+            carry=sum/10;
+            curr.data=sum%10;
+            prev=curr;
+            curr=curr.next;
+        }
+        if(carry == 1){
+            Node new_node=new Node(1);
+            prev.next=new_node;
+        }
+        return reverse(current1);
+    }
+}
--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -2,145 +2,69 @@ class Node{
     int data;
     Node next;
     Node(int data){
-        this.data=data;
+        this.data = data;
         this.next=null;
     }
 }
 public class LinkedList{
     Node head = null;
+
     public void insertAtFirst(int data){
-        Node newNode = new Node(data);
+        Node newNode  = new Node(data);
         newNode.next = head;
         head = newNode;
     }
 
-    public void insertAtPosition(int data,int pos){
+    public void insertAtPosition(int data, int pos){
         if(pos<0){
-            System.out.println("Negative Number : "+ pos);
-            return;
+            System.out.println("Negative position: "+ pos);
         }
-        if(pos == 0){
+        Node newNode = new Node(data);
+        if(pos==0){
             insertAtFirst(data);
             return;
         }
-
-        Node newNode  = new Node(data);
-        Node  current = head;
+        Node current = head;
         int count = 0;
-        while(current!=null && count<pos-1){
+        while(current != null && count<pos-1){
             current = current.next;
             count++;
         }
-        if(current==null){
-            System.out.println("Position out of range: " + pos);
-            return; 
+        if(current == null){
+            System.out.println("Position out of Range: "+pos);
         }
-        newNode.next=current.next;
-        current.next=newNode;
+        newNode.next = current.next;
+        current.next = newNode;
     }
 
     public void insertAtLast(int data){
         Node newNode = new Node(data);
         if(head == null){
-            head = newNode ;
+            head = newNode;
             return;
         }
         Node current = head;
-        while(current.next!=null){
-            current=current.next;
-        }
-        current.next=newNode;
-    }
-
-    public void removeFromFirst() {
-        if (head == null) {
-            System.out.println("List is Empty");
-            return;
-        }
-        head = head.next;
-    }
-    
-
-    public void removeFromPosition(int pos) {
-        if (head == null) {
-            System.out.println("List is Empty");
-            return;
-        }
-    
-        if (pos < 0) {
-            System.out.println("Negative Number: " + pos);
-            return;
-        }
-    
-        // If the position is 0, remove the head node.
-        if (pos == 0) {
-            head = head.next;
-            return;
-        }
-    
-        Node current = head;
-        int count = 0;
-    
-        // Traverse to the node before the desired position
-        while (current != null && count < pos - 1) {
-            current = current.next;
-            count++;
-        }
-    
-        // If current is null or current.next is null, position is out of range
-        if (current == null || current.next == null) {
-            System.out.println("Position Out of Range: " + pos);
-            return;
-        }
-    
-        // Remove the node at the desired position by skipping it
-        current.next = current.next.next;
-    }
-    
-
-    public void removeFromLast() {
-        // If the list is empty, there's nothing to remove
-        if (head == null) {
-            System.out.println("List is already empty.");
-            return;
-        }
-    
-        // If there's only one node, remove it by setting head to null
-        if (head.next == null) {
-            head = null;
-            return;
-        }
-    
-        // Traverse to the second-to-last node
-        Node current = head;
-        while (current.next.next != null) {
+        while(current.next != null){
             current = current.next;
         }
-    
-        // Set the next of the second-to-last node to null, removing the last node
-        current.next = null;
+        current.next = newNode;
     }
-    
+
     public void display(){
         Node current = head;
         while(current != null){
-            System.out.print(current.data+" --> ");
+            System.out.print(current.data + " --> ");
             current = current.next;
         }
         System.out.println("Null");
     }
 
-    
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
         ll.insertAtFirst(1);
-        ll.insertAtPosition(0,-1);
-        ll.insertAtPosition(0, 5);
-        ll.insertAtPosition(2, 0);
-        ll.insertAtLast(10);
-        ll.removeFromFirst();
-        ll.removeFromPosition(0);
-        ll.removeFromLast();
+        ll.insertAtPosition(2,1);
+        ll.insertAtPosition(0,0);
+        ll.insertAtLast(9);
         ll.display();
     }
 }
