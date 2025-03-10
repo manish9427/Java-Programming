2307a92c17f03793c50ddca39e8bf50735c43317
manish9427 <manish119427@gmail.com>
2025-03-10 14:31:20 +0530

    Reverse String

--- ReverseString.java
+++ ReverseString.java
@@ -1,5 +1,18 @@
 public class ReverseString{
     public static void main(String[] args) {
-        System.out.println("ReverseString");
+        String str = "Reverse String";
+
+        // Extra Space 
+        /*
+        String rev = "";
+        for(int i=str.length()-1;i>=0;i--){
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+         */
+
+         //In Place
+         
+
     }
 }
