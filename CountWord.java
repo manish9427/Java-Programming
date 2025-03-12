792b3133469878238a44fd9b0ebd5c6d085950eb
manish9427 <manish119427@gmail.com>
2025-03-12 14:33:09 +0530

    word Count

---
+++ CountWord.java
@@ -0,0 +1,7 @@
+public class CountWord {
+    public static void main(String args[]){
+        String str = "This is a count of Words";
+        String[] words = str.split(" ");
+        System.out.println(words.length);
+    }
+}
