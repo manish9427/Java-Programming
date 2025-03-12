4867cb4ea7d521a6e2b8abbf5f0db8a5b7464ad4
manish9427 <manish119427@gmail.com>
2025-03-12 15:14:23 +0530

    Count Word

--- CountWord.java
+++ CountWord.java
@@ -1,7 +1,17 @@
+import java .util.* ;
 public class CountWord {
     public static void main(String args[]){
         String str = "This is a count of Words";
         String[] words = str.split(" ");
         System.out.println(words.length);
+
+        //Using HashMap
+        int count = 0;
+        Map<String, Integer> wordCount = new HashMap<>();
+        for(String word: words){
+            wordCount.put(word, wordCount.getOrDefault(word, 0)+1);
+            count ++;
+        }
+        System.out.println(count);
     }
 }
