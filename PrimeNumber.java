4fbc3dde7df5855ee9323282f57de2809ce4c719
manish9427 <manish119427@gmail.com>
2025-03-16 14:27:37 +0530

    new

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,12 +1,19 @@
 public class PrimeNumber {
     public static void main(String args[]){
-        int num = 7;
+        int num = 717;
+        
+//   int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+
+//   for (int prime : primes) {
+//     if (num % prime == 0) System.out.println( num + " Not a prime number");
+//   }
         if(num <= 1) System.out.println( num + " Not a prime number"); 
         if (num <= 3) System.out.println(num + " a prime number"); 
         if (num % 2 == 0 || num % 3 == 0)  System.out.println( num + " Not a prime number"); 
-        while(num > 2){
-            if(num % 2 == 0) break;
-            num--;
+        for(int i=5;i*i<num;i=+6){
+            if(num%i==0 || num%(i+2)==0) {System.out.println(num + "is not Prime");} 
         }
+        System.out.println(num + " Prime Number");
     }
 }
