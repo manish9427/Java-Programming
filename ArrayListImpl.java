cf2fda1f98e8a55269accd4dde83b47a56cbffc9
manish9427 <manish119427@gmail.com>
2025-03-19 14:43:24 +0530

    Array List

--- ArrayListImpl.java
+++ ArrayListImpl.java
@@ -5,8 +5,21 @@ public class ArrayListImpl {
         al.add(10);
         al.add(20);
 
+        System.out.println("Using For Loop");
         for(int i=0;i<al.size();i++){
             System.out.println(al.get(i));
+        }
+
+        System.out.println("Using While Loop");
+        int j=0;
+        while(j<al.size()){
+            System.out.println(al.get(j));
+            j++;
+        }
+
+        System.out.println("Using Advanced loop");
+        for(Integer i:al){
+            System.out.println(i);
         }
         
     }
