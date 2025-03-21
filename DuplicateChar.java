71ed77ab7d295e5eb8a17d57431b94228338c440
manish9427 <manish119427@gmail.com>
2025-03-21 15:16:14 +0530

    Char Duplicate

---
+++ DuplicateChar.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class DuplicateChar{
+    public static void main(String args[]){
+        String str = "Mississippi";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<str.length();i++){
+        }
+    }
+}
+
+
+/*
+ String input = "Mississippi";
+
+i appears 4 times
+s appears 4 times
+p appears 2 times
+
+ */
