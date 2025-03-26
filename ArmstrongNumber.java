66b340ab6ab860e0154a4a28c21267f4739fd0c9
manish9427 <manish119427@gmail.com>
2025-03-26 14:58:21 +0530

    ArmStrong Number

---
+++ ArmstrongNumber.java
@@ -0,0 +1,18 @@
+public class ArmstrongNumber {
+    public static void main(String args[]){
+        int num = 1634; // 1 + 125 + 27 = 153
+        int sum = 0;
+        int temp = num;
+        int digits = String.valueOf(num).length();
+        while(num>0){
+            int digit = num %10;
+            sum += Math.pow(digit,digits);
+            num /= 10;
+        }
+        if(temp == sum){
+            System.out.println(temp + " is an Armstrong number.");
+        }else{
+            System.out.println(temp + " is not an Armstrong number.");
+        }
+    }
+}
