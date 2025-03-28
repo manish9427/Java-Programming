d5265d71235f82ee6829ad015b67c5c2f6cf3422
manish9427 <manish119427@gmail.com>
2025-03-28 15:01:21 +0530

    new

--- RemoveWhiteSpace.java
+++ RemoveWhiteSpace.java
@@ -1,7 +1,14 @@
 public class RemoveWhiteSpace {
     public static void main(String args[]){
         String str = "Man ish";
-        String ans = str.replaceAll(" ","");
+        String ans = str.replace("Man","Nam"); // O(n)
         System.out.println(ans);
+        String s = "Manish 123 Verma";
+        String ss = s.replaceAll("\\d+",""); 
+        System.out.println(ss);
+        String text = "   Hello, world!   ";
+        String trimmedText = text.trim();
+        System.out.println(trimmedText); 
+
     }
 }
