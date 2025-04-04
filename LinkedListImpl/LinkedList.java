987f2d1f83097a8932cd2cb23daff8f58944f9b8
manish9427 <manish119427@gmail.com>
2025-04-04 17:53:48 +0530

    remove

--- LinkedListImpl/LinkedList.java
+++ LinkedListImpl/LinkedList.java
@@ -3,7 +3,7 @@ package LinkedListImpl;
 class Node {
     int data;
     Node next;
-    
+
     Node(int data) {  
         this.data = data;
         this.next = null;
@@ -28,6 +28,32 @@ public class LinkedList {
             }
         }
 
+        // Remove by value (first occurrence)
+        public void remove(int data) {
+            if (head == null) return;  // Empty list
+
+            // Case 1: Remove head
+            if (head.data == data) {
+                head = head.next;
+                return;
+            }
+
+            // Case 2: Remove middle or tail
+            Node current = head;
+            while (current.next != null && current.next.data != data) {
+                current = current.next;
+            }
+
+            if (current.next != null) {
+                current.next = current.next.next;  // Bypass the node to remove
+            }else{
+                System.out.println(data + " Element not found in list.");
+                return;  // Element not found in list
+            }
+            
+        
+        }
+
         // Print the list
         public void show() {
             Node current = head;
@@ -48,6 +74,7 @@ public class LinkedList {
         list.insert(5);
         list.insert(0);
         list.insert(10);
+        list.remove(3);
         
         list.show();  
     }
