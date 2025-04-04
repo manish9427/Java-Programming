9f0996126bb873b864f4df192ddfbc3b8c2e56eb
manish9427 <manish119427@gmail.com>
2025-04-04 20:31:58 +0530

    new

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -2,7 +2,7 @@ public class ReverseNumber {
     public static void main(String args[]){
         int num = 123;
         int reversedNum = 0;
-        while(num>0){
+        while(num!=0){
             int digit = num % 10;
             reversedNum = reversedNum * 10 + digit;
             num /= 10;
---
+++ SlidingWindow/Longest_substring_without_repeating_characters.java
@@ -0,0 +1,28 @@
+package SlidingWindow;
+
+public class Longest_substring_without_repeating_characters {
+    public static void main(String[] args){
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+        s = "bbbbb";
+        System.out.println(lengthOfLongestSubstring(s));
+        s = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        if (n <= 1) return n;
+        int maxLength = 0, start = 0;
+        int[] charIndex = new int[256]; // Initialize all characters as -1
+        for (int i = 0; i < n; i++) {
+            int index = charIndex[s.charAt(i)];
+            if (index >= start) {
+                start = index + 1;
+            }
+            charIndex[s.charAt(i)] = i;
+            maxLength = Math.max(maxLength, i - start + 1);
+        }
+        return maxLength;
+    }
+        
+}
