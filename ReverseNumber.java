e0f31904a2921fe83edf3a0878491f27ee11b5e3
manish9427 <manish119427@gmail.com>
2025-05-11 22:17:25 +0530

    reverse num

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,12 +1,29 @@
 public class ReverseNumber {
     public static void main(String args[]){
         int num = 123;
+
+        // Using Number of Digits
+        /*
         int reversedNum = 0;
         while(num!=0){
             int digit = num % 10;
             reversedNum = reversedNum * 10 + digit;
             num /= 10;
         }
-        System.out.println(reversedNum);
+        System.out.println(reversedNum);         
+         */
+
+         // Using Convert
+
+        //  String str = String.valueOf(num);
+        String str = Integer.toString(num);
+        StringBuilder sb = new StringBuilder();
+        for(int i=str.length()-1;i>=0;i--){
+            sb.append(str.charAt(i));
+        }
+        String rev  = sb.toString();
+        int reverseNum = Integer.parseInt(rev);
+        System.out.println(reverseNum);
+
     }
 }
