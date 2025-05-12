17ce1908a98366e4c7b706dd4701ee8167d5fa05
manish9427 <manish119427@gmail.com>
2025-05-12 16:44:08 +0530

    hashmap

--- CountWord.java
+++ CountWord.java
@@ -2,13 +2,42 @@ import java .util.* ;
 public class CountWord {
     public static void main(String args[]){
         String str = "This is a count of Words";
-        String[] words = str.split("\\s+"); // Remove Extra Space
 
-        //Using HashMap
+        /*
+        String[] words = str.split("\\s+"); 
         Map<String, Integer> map = new HashMap<>();
         for(String word: words){
             map.put(word, map.getOrDefault(word, 0)+1);
         }
+        System.out.println(map);         
+         */
+
+         wordCount(str);
+    }
+    public static void wordCount(String str){
+        Map<String, Integer> map = new HashMap<>();
+        str = str.replaceAll("[^a-zA-Z0-9]"," ").toLowerCase();
+        String[] words = str.split("\\s+");
+        for(String word:words){
+            if(map.containsKey(word)){
+                int count = map.get(word);
+                map.put(word,count+1);
+            }else{
+                map.put(word,1);
+            }
+        }
         System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
+        for(String key:map.keySet()){
+            System.out.println(key + " : " + map.get(key));
+        }
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            System.out.println(entry);
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -7,8 +7,39 @@
 - StringBuilder sb = new StringBuilder()
 - sb.append(str.charAt(i))
 - System.out.println(sb.toString());
+- String str = "This is a count of Words";
+- String[] words = str.split("\\s+");  // Split the string into words using whitespace as the delimiter
+- str = str.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase(); // Remove punctuation and convert the string to lowercase
+- 
 
 # Convert 
 - String str = Integer.toString(num); //Convert the integer to a string
 - String str = String.valueOf(num); //Convert the integer to a string
 - int reversedNum = Integer.parseInt(reversedStr); // Convert the reversed string back to an integer
+
+# Array
+- String[] words = {"Manish","Verma"}; 
+- for(String word:words){}
+
+
+# HashMap
+- Map<key, value> map = new HashMap<>();
+- map.put(key,value)
+- map.getOrDefault(word, 0)+1
+- map.containsKey(word)
+- System.out.println(map) // object of {key=value}
+- map.get(word) // you will get the value of the key
+- map.keySet() // array of key
+- map.values() // array of values
+- map.entrySet() // array of [key=values]
+###### keySet
+for(String key:map.keySet()){
+    System.out.println(key + " : " + map.get(key));
+}
+###### entrySet
+for(Map.Entry<String, Integer> entry: map.entrySet()){
+    System.out.println(entry.getKey() + " : " + entry.getValue());
+}
+for(Map.Entry<String, Integer> entry: map.entrySet()){
+    System.out.println(entry);
+}
