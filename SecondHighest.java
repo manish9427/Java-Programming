271bf5b9bb991f3bd09c7746e0552ba61c8c638b
manish9427 <manish119427@gmail.com>
2025-05-16 14:51:54 +0530

    Second Heighest

--- SecondHighest.java
+++ SecondHighest.java
@@ -1,10 +1,11 @@
-public class LargestNum {
+public class SecondHighest {
     public static void main(String args[]){
         int[] arr = {3, 30, 34, 5, 9};
         largestNumber(arr);
     }
     public static void largestNumber(int[] nums){
-        int maxValue = 0;
+        int maxValue = Integer.MIN_VALUE;
+        System.out.println(maxValue);
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > maxValue) {
                 maxValue = nums[i];
